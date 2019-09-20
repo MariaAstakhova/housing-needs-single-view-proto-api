5fd519dbfba84028fa3817eebcab2a8dcd70407a
Ben Pirt <ben@pirt.co.uk>
2019-09-20 09:22:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3010
+const QueryHandler = require('./lib/QueryHandler');
+
+app.get('/customers', (req, res) => {
+
+  // Select which systems we want to query
+  let systems = ['SingleView', 'UHT', 'Jigsaw', 'Academy']
+  if(req.query.systems){
+    systems = req.query.systems.split(',');
+  }
+  
+  QueryHandler.customerSearch(req.query, systems, (results) => {
+    res.send(results)
+  });
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
