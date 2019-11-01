aa33a3a0e0aaa326f0ca92dd1ba8b5bd77ea7998
Matt Millar <matthew.millar@madetech.com>
2019-11-01 12:21:53 +0000

    Search on all systems from now on

--- index.js
+++ index.js
@@ -1,73 +1,38 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const host = "0.0.0.0";
+const host = '0.0.0.0';
 const port = 3000;
-const QueryHandler = require("./lib/QueryHandler");
-const { Systems } = require("./lib/Constants");
-const cors = require("cors");
+const QueryHandler = require('./lib/QueryHandler');
+const { Systems } = require('./lib/Constants');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-// maybe put this in a route so front end can get systems from api
-function extractSystems(incoming) {
-  let systems = [
-    Systems.SINGLEVIEW,
-    Systems.UHT_CONTACTS,
-    Systems.UHT_HOUSING_REGISTER,
-    Systems.UHW,
-    Systems.JIGSAW,
-    Systems.ACADEMY
-  ];
-  if (incoming.query.systems) {
-    systems = incoming.query.systems.split(",");
-  }
-  return systems;
-}
-
-app.get("/systems", async (req, res) => {
-  res.json([
-    Systems.SINGLEVIEW,
-    Systems.UHT_CONTACTS,
-    Systems.UHT_HOUSING_REGISTER,
-    Systems.UHW,
-    Systems.JIGSAW,
-    Systems.ACADEMY
-  ]);
-});
-
-app.get("/customers", async (req, res) => {
-  // Select which systems we want to query
-  const systems = extractSystems(req);
-  const results = await QueryHandler.searchCustomers(req.query, systems);
+app.get('/customers', async (req, res) => {
+  const results = await QueryHandler.searchCustomers(req.query);
   res.send(results);
 });
 
-app.post("/customers", async (req, res) => {
+app.post('/customers', async (req, res) => {
   // Save the selected customer records
   const id = await QueryHandler.saveCustomer(req.body);
   res.redirect(`/customers/${id}`);
 });
 
-app.get("/customers/:id", async (req, res) => {
-  const systems = extractSystems(req);
-  const result = await QueryHandler.fetchCustomer(req.params.id, systems);
+app.get('/customers/:id', async (req, res) => {
+  const result = await QueryHandler.fetchCustomer(req.params.id);
   res.send({ customer: result });
 });
 
-app.get("/customers/:id/notes", async (req, res) => {
-  const systems = extractSystems(req);
-  const results = await QueryHandler.fetchCustomerNotes(req.params.id, systems);
+app.get('/customers/:id/notes', async (req, res) => {
+  const results = await QueryHandler.fetchCustomerNotes(req.params.id);
   res.send(results);
 });
 
-app.get("/customers/:id/documents", async (req, res) => {
-  const systems = extractSystems(req);
-  const results = await QueryHandler.fetchCustomerDocuments(
-    req.params.id,
-    systems
-  );
+app.get('/customers/:id/documents', async (req, res) => {
+  const results = await QueryHandler.fetchCustomerDocuments(req.params.id);
   res.send(results);
 });
 
