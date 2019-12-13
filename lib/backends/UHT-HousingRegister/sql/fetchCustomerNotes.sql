8f0ebba2eeaae30131ea3b7f02382c8d30fdb396
Ben Pirt <ben@pirt.co.uk>
2019-12-13 16:22:58 +0000

    Get username for notes in UHT Housing Register

--- lib/backends/UHT-HousingRegister/sql/fetchCustomerNotes.sql
+++ lib/backends/UHT-HousingRegister/sql/fetchCustomerNotes.sql
@@ -1,9 +1,12 @@
 SELECT
-	*
+	conlog.clog_details,
+	conlog.clog_date,
+	auser.username
 FROM
 	conlog
 	JOIN wlapp ON wlapp.con_key = conlog.con_key
 	JOIN wlmember ON wlmember.app_ref = wlapp.app_ref
+	JOIN auser ON auser.user_code = conlog.user_code
 WHERE
 	wlmember.app_ref = @app_ref
 	AND wlmember.person_no = @person_no
